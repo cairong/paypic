fc8fb42416f6e5c5e9420a2aa3f9456071c2461a
pay <cairong1977@126.com>
2021-09-29 12:18:27 +0800

    Add files via upload

---
+++ reply.php
@@ -0,0 +1,8 @@
+return reply(
+     [
+          `早上好`=>`good morning`
+          `壁纸` => `https://acg.toubiec.cn/random.php`
+          `帮助`=>`请自行开启以下活动并完成新手任务：京东APP（东东工厂、东东农场、宠汪汪、东东萌宠、东东超市、口袋书店），京喜APP（京喜工厂、京喜农场、京喜财富岛、京喜牧场），京东极速版APP（汪汪乐园）`
+          `活动指南`=>`https://ghproxy.com/https://github.com/U188/JDC/blob/master/sm.jpg`
+     ]
+)
